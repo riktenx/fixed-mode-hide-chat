bfc841fdfbb3f484c6be6e0050d8193b5d2b6bf2
oont <hunterisgreen@gmail.com>
2025-02-16 13:58:04 -0600

    Remove missed unused imports

--- src/main/java/io/github/deathbeam/plugins/fixedhidechat/FixedHideChatPlugin.java
+++ src/main/java/io/github/deathbeam/plugins/fixedhidechat/FixedHideChatPlugin.java
@@ -1,13 +1,10 @@
 package io.github.deathbeam.plugins.fixedhidechat;
 
-import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.util.*;
 import javax.inject.Inject;
-import javax.swing.text.AbstractDocument;
 
 import com.google.inject.*;
-import jdk.jfr.ContentType;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.api.events.BeforeRender;
